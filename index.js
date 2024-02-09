c7ab323ac64793af3c590e9be89f6553f8f84941
Sahil Sanghar <151014159+SahilSanghar@users.noreply.github.com>
2024-02-09 14:27:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,11 +24,58 @@ app.get("/", (req, res) => {
     res.send("root is working")
 });
 
+//Index route
 app.get("/chats", async (req, res) => {
     let Chats = await Chat.find();
     console.log(Chats);
     res.render("index.ejs", { Chats })
 });
+
+//New route
+app.get("/chats/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+//Create route
+app.post("/chats", (req, res) => {
+    let { from, msg, to } = req.body;
+    let newChat = new Chat ({
+        from: from,
+        msg: msg,
+        to: to,
+        created_at: new Date(),
+    });
+    newChat.save()
+    .then((res) => {
+        console.log("chat saved")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+    res.redirect("/chats")
+})
+
+//Edit route
+app.get("/chats/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id)
+    res.render("edit.ejs", { chat })
+})
+
+//Update route
+app.put("chats/:id", async (req, res) => {
+    let {id} = req.params;
+    let { msg: newMsg } = req.body;
+    console.log(newMsg)
+    let updatedChat = await Chat.findByIdAndUpdate(id,
+        { msg: newMsg},
+        {runValidators: true, new: true},
+        { new: true },
+        )
+        console.log(updatedChat);
+        res.redirect("/chats");
+})
+
 app.listen(8080, () => {
     console.log("Server is listening to port 8080")
 });
